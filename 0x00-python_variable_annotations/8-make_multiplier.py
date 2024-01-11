6ed779a641e50d55f2f08a304a943a17ac499eed
TobiLight <jgetitdonefast@gmail.com>
2024-01-11 21:54:02 +0100

    ✔ Returns a function that multiplies a float.

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# File: 8-make_multiplier.py
+# Author: Oluwatobiloba Light
+# A type-annotated function make_multiplier that takes a float
+# multiplier as argument and returns a function that multiplies a float
+# by multiplier.
+"""
+Multiplier module
+"""
+
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies a float.
+    """
+    def wrapper(value: float = multiplier) -> float:
+        return value * value
+
+    return wrapper
