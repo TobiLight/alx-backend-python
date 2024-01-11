7f06322890ce58d554c004191b5728f00f6f9a8c
TobiLight <jgetitdonefast@gmail.com>
2024-01-11 22:44:47 +0100

    ✔ Returns a list

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# File: 102-type_checking.py
+# Author: Oluwatobiloba Light
+# Validates the following piece of code and apply any necessary changes.
+"""
+Zoom Array module
+"""
+
+
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Returns a list.
+    """
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array: Tuple = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x: List = zoom_array(array, 3)
