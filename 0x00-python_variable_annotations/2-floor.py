5e543a0bc9a308858136b6b50915f88edc7da8a5
TobiLight <jgetitdonefast@gmail.com>
2024-01-11 18:43:24 +0100

    ✔ Returns the floor of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# File: 2-floor.py
+# Author: Oluwatobiloba Light
+# A type-annotated function floor which takes a float n as argument and
+# returns the floor of the float.
+
+
+import math
+
+
+def floor(n: float) -> float:
+	"""
+	Returns the floor of the float.
+	"""
+	return math.floor(n)
