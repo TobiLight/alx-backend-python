6fbd1d11215fec66455d0efed56dc96628860d6f
TobiLight <jgetitdonefast@gmail.com>
2024-01-11 19:20:07 +0100

    ✔ Returns the sum of a list as a float.

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# File: 5-sum_list.py
+# Author: Oluwatobiloba Light
+# A type-annotated function sum_list which takes a list input_list of
+# floats as argument and returns their sum as a float.
+"""
+Sum list module
+"""
+
+
+def sum_list(input_list: list) -> float:
+    """
+    Returns their sum as a float.
+    """
+    return sum(input_list)
