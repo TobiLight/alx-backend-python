99cee6d3769ddae128181da0a63a3a3f845f3024
TobiLight <jgetitdonefast@gmail.com>
2024-01-11 19:13:29 +0100

    ✔ Define and annotate the following variables with the specified values:

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# File: 4-define_variables.py
+# Author: Oluwatobiloba Light
+# Define and annotate the following variables with the specified values:
+"""
+Task 4 module
+"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
