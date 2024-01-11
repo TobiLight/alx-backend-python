95b9af755354b9c885a9d7476ce447912f781c5b
TobiLight <jgetitdonefast@gmail.com>
2024-01-11 22:15:48 +0100

    ✔ Augments the following code with the correct duck-typed annotations

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# File: 100-safe_first_element.py
+# Author: Oluwatobiloba Light
+# Augments the following code with the correct duck-typed annotations
+"""
+Safe first element module
+"""
+
+
+from typing import Any, Sequence, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
