74e34af84da11658bd9335789b2fa6c0b126b505
TobiLight <jgetitdonefast@gmail.com>
2024-01-11 19:00:41 +0100

    ✔ Returns a concatenated string

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,6 +3,9 @@
 # Author: Oluwatobiloba Light
 # A  type-annotated function concat that takes a string str1 and a
 # string str2 as arguments and returns a concatenated string
+"""
+Concat module
+"""
 
 
 def concat(str1: str, str2: str) -> str:
