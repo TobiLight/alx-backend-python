db0cfa69ab03ea6ba4d29ad2cb9030eba7d63d3f
TobiLight <jgetitdonefast@gmail.com>
2024-01-11 21:13:55 +0100

    ✔ Returns a tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# File: 7-to_kv.py
+# Author: Oluwatobiloba Light
+# A type-annotated function to_kv that takes a string k and an int OR
+# float v as arguments and returns a tuple.
+"""
+Task 7 module
+"""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+    """
+    Returns a tuple
+    """
+    return k, v ** 2.0
