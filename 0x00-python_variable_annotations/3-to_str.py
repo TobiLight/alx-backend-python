17035f5ab72275e524a43e4b5ad439cfac8c582b
TobiLight <jgetitdonefast@gmail.com>
2024-01-11 19:09:53 +0100

    ✔ Returns the string representation of the float.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# File: 3-to_str.py
+# Author: Oluwatobiloba Light
+# Atype-annotated function to_str that takes a float n as argument and returns
+# the string representation of the float.
+"""
+String repr module
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of the float.
+    """
+    return str(n)
