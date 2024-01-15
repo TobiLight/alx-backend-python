70d614fe599d52dbc935ebc221a1741a68715bec
TobiLight <jgetitdonefast@gmail.com>
2024-01-15 21:54:28 +0100

    ✔ Returns an asyncio task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# File: 3-tasks.py
+# Author: Oluwatobiloba Light
+""""
+Task wait random module
+"""
+
+
+from asyncio import Task
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Task:
+    """
+    Returns an asyncio.Task
+    """
+    return asyncio.create_task(wait_random(max_delay))
