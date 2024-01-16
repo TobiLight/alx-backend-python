041b452a1b2cf96acfdbcf648269970e5b7da97a
TobiLight <jgetitdonefast@gmail.com>
2024-01-16 19:58:20 +0100

    ✔ An asynchronous generator coroutine that yields random numbers.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,7 +4,7 @@
 """Task 0 module"""
 
 import asyncio
-from random import uniform
+from random import randint, uniform
 
 
 async def async_generator():
@@ -13,4 +13,4 @@ async def async_generator():
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield uniform(0, 10)
+        yield randint(0, 10)
