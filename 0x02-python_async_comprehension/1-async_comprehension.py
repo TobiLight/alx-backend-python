40f813fcdb47316025a22855ef5b5c9212e3ed35
TobiLight <jgetitdonefast@gmail.com>
2024-01-16 21:10:07 +0100

    ✔ Creates a list of 10 numbers from a 10-number generator.

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# File: 1-async_comprehension.py
+# Author: Oluwatobiloba Light
+"""Task 1 module"""
+
+from typing import Generator
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[float, None, None]:
+    """
+    Creates a list of 10 numbers from a 10-number generator.
+    """
+    ret = [number async for number in async_generator()]
+    return ret
