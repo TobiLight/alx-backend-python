4674b39ddc9929a4ec4569eb8b5380cce704cd11
TobiLight <jgetitdonefast@gmail.com>
2024-02-05 20:59:21 +0100

    ✔ Fix: Tests that utils.get_json returns the expected result.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -41,10 +41,10 @@ class TestGetJSON(unittest.TestCase):
         ('http://example.com', {'payload': True}),
         ('http://holberton.io', {'payload': False})
     ])
-    def test_get_json(self, test_url: str, payload: Dict) -> None:
+    def test_get_json(self, test_url: str, test_payload: Dict) -> None:
         """Tests that utils.get_json returns the expected result."""
-        json_return = {'json.return_value': payload}
+        json_return = {'json.return_value': test_payload}
         with patch('utils.requests.get', return_value=Mock(**json_return)) as\
                 mock_get:
-            self.assertEqual(get_json(test_url), payload)
+            self.assertEqual(get_json(test_url), test_payload)
             mock_get.assert_called_once_with(test_url)
