f20db2094e3191146110504a731cf6bb8cad1978
TobiLight <jgetitdonefast@gmail.com>
2024-02-05 01:20:57 +0100

    ✔ feat: Test access nested map with exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,15 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test Access nested map with key path"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ('a', ), KeyError('a')),
+        ({'a': 1}, ('a', 'b'), KeyError('b')),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Test Access nested map with exception"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        exception_message = str(context.exception)
+        self.assertEqual(exception_message, str(expected))
