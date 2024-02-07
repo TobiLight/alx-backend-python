c591a740b6ef4f1639106177bbc679661ec0e6d1
TobiLight <jgetitdonefast@gmail.com>
2024-02-07 03:53:03 +0100

    Performs integration tests for the  class.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -137,14 +137,14 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         """Tests the `public_repos` method."""
         self.assertEqual(
             GithubOrgClient("google").public_repos(),
-            self.expected_repos,
+            cls.expected_repos,
         )
 
     def test_public_repos_with_license(self) -> None:
         """Tests the `public_repos` method with a license."""
         self.assertEqual(
             GithubOrgClient("google").public_repos(license="apache-2.0"),
-            self.apache2_repos,
+            cls.apache2_repos,
         )
 
     @classmethod
