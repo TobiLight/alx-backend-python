7c1e6d1050ffa0627966aa8e261af3379e3be21f
TobiLight <jgetitdonefast@gmail.com>
2024-02-10 02:02:27 +0100

    ✔ fix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -28,31 +28,76 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_result.assert_called_once_with(
             f'https://api.github.com/orgs/{test_org_name}')
 
-    @patch("client.GithubOrgClient.org", new_callable=PropertyMock)
-    def test_public_repos_url(self, mock_org) -> None:
-        """doc doc doc"""
-        payload = {"repos_url": "https://api.github.com/orgs/google/repos"}
-        mock_org.return_value = payload
-        github_org_client = GithubOrgClient("google")
-        self.assertEqual(github_org_client._public_repos_url,
-                         payload["repos_url"])
+    def test_public_repos_url(self):
+        """
+        Tests that the result of _public_repos_url is the expected one based
+        on the mocked payload.
+        """
+        with patch("client.GithubOrgClient.org", new_callable=PropertyMock)\
+                as mock_result:
+            mock_result.return_value = {
+                "repos_url": "https://api.github.com/orgs/google/repos"}
+            github_client = GithubOrgClient("google")
+            self.assertEqual(github_client._public_repos_url,
+                             "https://api.github.com/orgs/google/repos")
 
-    @patch("client.get_json",
-           return_value=[{"name": "repo1"}, {"name": "repo2"}])
-    def test_public_repos(self, mock_get_json) -> None:
-        """doc doc doc"""
-        with patch(
-            "client.GithubOrgClient._public_repos_url",
-            new_callable=PropertyMock
-        ) as mock_public_repos_url:
-            mock_public_repos_url.return_value = (
+    @patch("client.get_json", return_value=[{
+        "name": "hello"
+    }, {"name": "world"}])
+    def test_public_repos(self, mock_json) -> None:
+        """
+        Tests that the list of repos is what you expect from the chosen
+        payload.
+        """
+        test_payload = {
+            'repos_url': "https://api.github.com/users/google/repos",
+            'repos': [
+                {
+                    "id": 7697149,
+                    "name": "episodes.dart",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/episodes.dart",
+                    "created_at": "2013-01-19T00:31:37Z",
+                    "updated_at": "2019-09-23T11:53:58Z",
+                    "has_issues": True,
+                    "forks": 22,
+                    "default_branch": "master",
+                },
+                {
+                    "id": 8566972,
+                    "name": "kratu",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/kratu",
+                    "created_at": "2013-03-04T22:52:33Z",
+                    "updated_at": "2019-11-15T22:22:16Z",
+                    "has_issues": True,
+                    "forks": 32,
+                    "default_branch": "master",
+                },
+            ]
+        }
+
+        with patch("client.GithubOrgClient._public_repos_url",
+                   new_callable=PropertyMock) as mock_result:
+            mock_result.return_value =\
                 "https://api.github.com/orgs/google/repos"
-            )
-            github_org_client = GithubOrgClient("google")
-            self.assertEqual(github_org_client.public_repos(),
-                             ["repo1", "repo2"])
-            mock_get_json.assert_called_once()
-        mock_public_repos_url.assert_called_once()
+            github_client = GithubOrgClient("google").public_repos()
+            self.assertEqual(github_client, [
+                "hello",
+                "world",
+            ])
+            mock_result.assert_called_once()
+        mock_json.assert_called_once()
 
     @parameterized.expand([
         ({'license': {'key': "bsd-3-clause"}}, "bsd-3-clause", True),
